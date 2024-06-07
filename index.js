abb461f1839a61761fc99faa59badc2927689de5
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2024-06-07 20:08:28 +0100

    Use route middleware

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const staticFileRouter = require("./routes/staticRoutes");
+const urlShortenerRouter = require("./routes/urlShortenerRoutes");
 
 // Middleware
 app.use(cors());
 app.use("/public", express.static(`${process.cwd()}/public`));
+app.use(express.urlencoded({ extended: true }));
 
 // Router middleware
 app.use("/", staticFileRouter);
+app.use("/api/shorturl", urlShortenerRouter);
 
 module.exports = app;
