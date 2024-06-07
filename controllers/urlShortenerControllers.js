48d1410b1d4d7f8dc56b74e76b186650fccbd9d2
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2024-06-07 21:01:01 +0100

    Create local url doc

--- controllers/urlShortenerControllers.js
+++ controllers/urlShortenerControllers.js
@@ -1,4 +1,5 @@
 const URL = require("url").URL;
+const ShortUniqueId = require("short-unique-id");
 
 const validateUrl = (input) => {
   try {
@@ -19,5 +20,18 @@ exports.shortenUrl = (req, res) => {
   }
 
   try {
-  } catch (error) {}
+    const { randomUUID } = new ShortUniqueId({ length: 10 });
+    const short_url = randomUUID();
+
+    const urlDoc = {
+      original_url,
+      short_url,
+    };
+
+    res.status(201).json(urlDoc);
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
+    });
+  }
 };
