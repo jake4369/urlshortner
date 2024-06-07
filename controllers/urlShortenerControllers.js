4d2ef359d1bc993eb17e07d6e3247c19b82840a0
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2024-06-07 20:53:49 +0100

    Validate url

--- controllers/urlShortenerControllers.js
+++ controllers/urlShortenerControllers.js
@@ -1,3 +1,23 @@
+const URL = require("url").URL;
+
+const validateUrl = (input) => {
+  try {
+    const url = new URL(input);
+    return url.protocol === "http:" || url.protocol === "https:";
+  } catch (error) {
+    return false;
+  }
+};
+
 exports.shortenUrl = (req, res) => {
-  console.log(req.body.url);
+  const original_url = req.body.url;
+
+  if (!validateUrl(original_url)) {
+    return res.status(400).json({
+      error: "invalid url",
+    });
+  }
+
+  try {
+  } catch (error) {}
 };
