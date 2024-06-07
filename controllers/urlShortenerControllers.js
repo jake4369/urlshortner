315a55a074ac1697be8ebf9acb4f0a633e07176e
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2024-06-07 23:52:26 +0100

    Remove depreciated urlparser.parse()

--- controllers/urlShortenerControllers.js
+++ controllers/urlShortenerControllers.js
@@ -1,87 +1,48 @@
-// const URL = require("url").URL;
-const urlparser = require("url");
+const URL = require("url").URL;
 const dns = require("dns");
 const ShortUniqueId = require("short-unique-id");
 const UrlDoc = require("./../models/urlDocModel");
 
-// try {
-//   const url = new URL(input);
-//   const isValidProtocol =
-//     url.protocol === "http:" || url.protocol === "https:";
-//   const isValidHostname = url.hostname && url.hostname.includes(".");
-//   return isValidProtocol && isValidHostname;
-// } catch (error) {
-//   return false;
-// }
-
-// const validateUrl = async (url) => {
-//   const regex = /^https?:\/\//;
-
-//   if (!regex.test(url)) {
-//     return false;
-//   }
-
-//   let formattedUrl = url.replace(regex, "");
-//   formattedUrl = formattedUrl.replace(/\/$/, "");
-
-//   return new Promise((resolve, reject) => {
-//     dns.lookup(formattedUrl, (err) => {
-//       if (err) {
-//         resolve(false);
-//       } else {
-//         resolve(true);
-//       }
-//     });
-//   });
-// };
-
-exports.shortenUrl = async (req, res) => {
+exports.shortenUrl = (req, res) => {
   const original_url = req.body.url;
 
-  const dnsLookup = dns.lookup(
-    urlparser.parse(original_url).hostname,
-    async (err, address) => {
-      if (!address) {
-        res.status(400).json({
-          error: "invalid url",
+  const hostname = new URL(original_url).hostname;
+
+  const dnsLookup = dns.lookup(hostname, async (err, address) => {
+    if (!address) {
+      res.status(400).json({
+        error: "invalid url",
+      });
+    } else {
+      try {
+        const { randomUUID } = new ShortUniqueId({ length: 10 });
+        const short_url = randomUUID();
+
+        const savedUrlDoc = await UrlDoc.create({
+          original_url,
+          short_url,
         });
-      } else {
-        try {
-          const { randomUUID } = new ShortUniqueId({ length: 10 });
-          const short_url = randomUUID();
 
-          const savedUrlDoc = await UrlDoc.create({
-            original_url,
-            short_url,
-          });
+        const result = await UrlDoc.findOne({
+          original_url: savedUrlDoc.original_url,
+        }).select("-_id -__v");
 
-          const result = await UrlDoc.findOne({
-            original_url: savedUrlDoc.original_url,
+        res.status(201).json(result);
+      } catch (error) {
+        if (error.code === 11000) {
+          const urlDoc = await UrlDoc.findOne({
+            original_url: req.body.url,
           }).select("-_id -__v");
 
-          res.status(201).json(result);
-        } catch (error) {
-          if (error.code === 11000) {
-            const urlDoc = await UrlDoc.findOne({
-              original_url: req.body.url,
-            }).select("-_id -__v");
-
-            return res.status(200).json(urlDoc);
-          }
-
-          res.status(500).json({
-            error: error.message,
-          });
+          return res.status(200).json(urlDoc);
         }
+
+        res.status(500).json({
+          error: error.message,
+        });
       }
     }
-  );
-
-  // if (!isValid) {
-  //   return res.status(400).json({
-  //     error: "invalid url",
-  //   });
-  // }
+  });
 };
 
 exports.redirectToOriginalUrl = async (req, res) => {
