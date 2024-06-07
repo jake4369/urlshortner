565fe26f88b9be4eb667a4b402dd560a3b47bfd5
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2024-06-07 19:23:56 +0100

    Import static file router

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const staticFileRouter = require("./routes/staticRoutes");
 
 // Middleware
-
 app.use(cors());
-
 app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get("/", function (req, res) {
-  res.sendFile(process.cwd() + "/views/index.html");
-});
+// Router middleware
+app.use("/", staticFileRouter);
 
 module.exports = app;
